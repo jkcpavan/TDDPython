6aaee5d6ecfc8c0158a0190691d2349dd7b2a16f
PXY6797 <PXY6797@WKR90Q47L4>
2019-08-12 15:37:01 -0400

    header Checks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from os import environ
-from flask import Flask
+from flask import Flask,request,abort
 from qrcodepkg.qrcode import qrcode
 
 app = Flask(__name__)
@@ -8,10 +8,14 @@ app.config.from_object(os.getenv('APP_SETTINGS','config.DevelopmentConfig'))
 
 @app.route('/')
 def hello():
+    if request.headers['x_forwarded_proto'].lower()== "http":
+       return abort(404)
     return 'Hello, World!'
 
 @app.route('/<msg>')
 def createqr(msg):
+    if request.headers['x_forwarded_proto'].lower()== "http":
+       return abort(404)
     return msg+'PNG'
 
 if __name__ == '__main__':
--- integration-test.py
+++ integration-test.py
@@ -7,14 +7,20 @@ class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
+        tester.environ_base['HTTP_X_FORWARDED_PROTO'] = 'HTTPS'
+        response = tester.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
     def test_create_qr(self):
         tester = app.test_client(self)
-        response = tester.get('/SampleImg', content_type='html/text')
+        tester.environ_base['HTTP_X_FORWARDED_PROTO'] = 'HTTPS'
+        response = tester.get('/SampleImg')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'PNG',response.data)
-
+    def test_index_https_check(self):
+        tester = app.test_client(self)
+        tester.environ_base['HTTP_X_FORWARDED_PROTO'] = 'HTTP'
+        self.assertEqual(tester.get('/').status_code, 404)
+        
 if __name__ == '__main__':
     unittest.main()
